a9ab01458a44624e959366e3f87176ec5e92bb96
darkogolovic <golovicdarko@gmail.com>
2025-11-23 21:18:05 +0100

    Ready

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,20 +4,19 @@ const mongoose = require('mongoose')
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const authController = require('./controllers/auth.js')
-const userController = require('./controllers/user.js')
-const postsController = require('./controllers/posts.js')
-const likeCommentController= require('./controllers/likes-comments.js')
-const conversationsController = require('./controllers/conversation.js')
+const authController = require('../../controllers/auth.js')
+const userController = require('../../controllers/user.js')
+const postsController = require('../../controllers/posts.js')
+const likeCommentController= require('../../controllers/likes-comments.js')
+const conversationsController = require('../../controllers/conversation.js')
 const http = require("http");
 const server = http.createServer(app);
-const socketServer = require("./socket/socket.js");
-const messagesContorller= require('./controllers/messages.js')
+const socketServer = require("../../socket/socket.js");
+const messagesContorller= require('../../controllers/messages.js')
 const serverless = require('serverless-http')
 
 
 app.use(cors());
-app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -35,6 +34,15 @@ app.use('/conversations',conversationsController);
 app.use('/messages',messagesContorller)
 
 socketServer(server);
-server.listen(3000,()=>{
-    console.log('server running on port 3000')
-})
+
+export const handler = serverless(server, {
+  request: (req, event) => {
+    if (typeof event.body === 'string') {
+      try {
+        req.body = JSON.parse(event.body);
+      } catch (err) {
+        req.body = {};
+      }
+    }
+  }
+});
