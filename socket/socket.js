873e0988fd2371301ad26e85fa5703a18c09a610
darkogolovic <golovicdarko@gmail.com>
2025-11-23 22:23:01 +0100

    cors fix

--- socket/socket.js
+++ socket/socket.js
@@ -3,7 +3,11 @@ const { Server } = require("socket.io");
 module.exports = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "https://circle-sociall.netlify.app"
+
+      ],
       methods: ["GET", "POST"],
     },
   });
